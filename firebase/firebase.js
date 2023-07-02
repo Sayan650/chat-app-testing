d130ee121fff0694394a605bb35a0ca153002389
Sayan Paul <sayanp650@gmail.com>
2023-07-02 22:32:58 +0530

    demo done

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyC3Vg4rUEC0xMc1zK9pnaHE75m78Fv4YxQ",
-    authDomain: "chat-app-4dc43.firebaseapp.com",
-    projectId: "chat-app-4dc43",
-    storageBucket: "chat-app-4dc43.appspot.com",
-    messagingSenderId: "397615249798",
-    appId: "1:397615249798:web:8495a28da2fd35f024d407",
-};
+    apiKey: "AIzaSyBmupVJh9AoIiRU-dAAMCmjZe9UwHbcJeQ",
+    authDomain: "chat-app-demo-887c6.firebaseapp.com",
+    projectId: "chat-app-demo-887c6",
+    storageBucket: "chat-app-demo-887c6.appspot.com",
+    messagingSenderId: "589050375858",
+    appId: "1:589050375858:web:4fd20f317b969e0d8e23ab",
+    measurementId: "G-XSLFJ0EE9R"
+  };
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
